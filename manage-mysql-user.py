883300c4cc5b24751e83f4d0def1f879ad69dc18
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-07-06 17:31:46 +0100

    Add AWS Secret Manager support

--- manage-mysql-user.py
+++ manage-mysql-user.py
@@ -6,12 +6,9 @@ import mysql.connector.pooling
 import boto3
 import random
 import string
-
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch
+import json
 
 libraries = ("boto3", "mysql")
-patch(libraries)
 
 # Initialise logging
 logger = logging.getLogger(__name__)
@@ -19,53 +16,81 @@ log_level = os.environ["LOG_LEVEL"] if "LOG_LEVEL" in os.environ else "INFO"
 logger.setLevel(logging.getLevelName(log_level.upper()))
 logger.info("Logging at {} level".format(log_level.upper()))
 
+# Use appropriate CA SSL cert to verify RDS identity. Use "AmazonRootCA1.pem" for Aurora Serverless.
+# Defaults to "rds-ca-2019-2015-root.pem" to suit normal RDS.
+if "RDS_CA_CERT" in os.environ:
+    rds_ca_cert = os.environ["RDS_CA_CERT"]
+else:
+    rds_ca_cert = "/var/task/rds-ca-2019-2015-root.pem"
+
 
 def generate_password():
     valid_chars = string.ascii_letters + string.digits + string.punctuation
     invalid_chars = ["/", "@", '"', "\\", "'"]  # Not allowed in a MySQL password
     pw_chars = "".join([i for i in valid_chars if i not in invalid_chars])
     pw = "".join((random.choice(pw_chars)) for x in range(40))
+    logger.debug("Password generated")
     return pw
 
 
-def update_password_parameter(password, parameter_name):
-    ssm = boto3.client("ssm")
-    try:
-        ssm.put_parameter(
-            Name=parameter_name, Type="SecureString", Value=password, Overwrite=True
+def update_password_source(password, password_source, password_source_type):
+    if password_source_type is "ssm":
+        ssm = boto3.client("ssm")
+        try:
+            ssm.put_parameter(
+                Name=password_source,
+                Type="SecureString",
+                Value=password,
+                Overwrite=True,
+            )
+            logger.debug(f"Password updated in {password_source_type}")
+        except Exception as e:
+            logger.error(e)
+    elif password_source_type is "secretsmanager":
+        secretsmanager = boto3.client("secretsmanager")
+        try:
+            secret_value = {"password": password}
+            secretsmanager.put_secret_value(
+                SecretId=password_source, SecretString=json.dumps(secret_value)
+            )
+            logger.debug(f"Password updated in {password_source_type}")
+        except Exception as e:
+            logger.error(e)
+    else:
+        raise Exception(f"Unknown password source type: {password_source_type}")
+
+
+def get_mysql_password(password_source, password_source_type):
+    if password_source_type is "ssm":
+        ssm = boto3.client("ssm")
+        return ssm.get_parameter(Name=password_source, WithDecryption=True)[
+            "Parameter"
+        ]["Value"]
+    elif password_source_type is "secretsmanager":
+        secretsmanager = boto3.client("secretsmanager")
+        secret_string_json = json.loads(
+            secretsmanager.get_secret_value(SecretId=password_source)["SecretString"]
         )
-    except Exception as e:
-        logger.error(e)
+        password = secret_string_json["password"]
+        return password
+    else:
+        raise Exception(f"Unknown password source type: {password_source_type}")
 
 
-def get_master_password():
-    ssm = boto3.client("ssm")
-    return ssm.get_parameter(
-        Name=os.environ["RDS_MASTER_PASSWORD_PARAMETER_NAME"], WithDecryption=True
-    )["Parameter"]["Value"]
-
-
-def get_mysql_password(parameter_name):
-    ssm = boto3.client("ssm")
-    return ssm.get_parameter(Name=parameter_name, WithDecryption=True)["Parameter"][
-        "Value"
-    ]
-
-
-def get_connection(password):
+def get_connection(username, password):
     return mysql.connector.connect(
         host=os.environ["RDS_ENDPOINT"],
-        user=os.environ["RDS_MASTER_USERNAME"],
+        user=username,
         password=password,
         database=os.environ["RDS_DATABASE_NAME"],
-        ssl_ca="/var/task/rds-ca-2019-2015-root.pem",
+        ssl_ca=rds_ca_cert,
         ssl_verify_cert=True,
     )
 
 
-def execute_statement(sql):
+def execute_statement(sql, username, password_source, password_source_type):
     connection = get_connection(
-        get_mysql_password(os.environ["RDS_MASTER_PASSWORD_PARAMETER_NAME"])
+        username, get_mysql_password(password_source, password_source_type)
     )
     logger = logging.getLogger()
     cursor = connection.cursor()
@@ -74,9 +99,9 @@ def execute_statement(sql):
     connection.close()
 
 
-def execute_query(sql):
+def execute_query(sql, username, password_source, password_source_type):
     connection = get_connection(
-        get_mysql_password(os.environ["RDS_MASTER_PASSWORD_PARAMETER_NAME"])
+        username, get_mysql_password(password_source, password_source_type)
     )
     logger = logging.getLogger()
     cursor = connection.cursor()
@@ -87,9 +112,12 @@ def execute_query(sql):
     return result
 
 
-def check_user_exists(username):
+def check_user_exists(master_username, username, password_source, password_source_type):
     result = execute_query(
-        "SELECT user FROM mysql.user WHERE user = '{}';".format(username)
+        "SELECT user FROM mysql.user WHERE user = '{}';".format(username),
+        master_username,
+        password_source,
+        password_source_type,
     )
     if len(result) > 0:
         if username in result[0]:
@@ -104,15 +132,15 @@ def check_user_exists(username):
         return False
 
 
-def test_connection(username, password_parameter):
-    mysql_user_password = get_mysql_password(password_parameter)
+def test_connection(username, password_source, password_source_type):
+    mysql_user_password = get_mysql_password(password_source, password_source_type)
     try:
         connection = mysql.connector.connect(
             host=os.environ["RDS_ENDPOINT"],
             user=username,
             password=mysql_user_password,
             database=os.environ["RDS_DATABASE_NAME"],
-            ssl_ca="/var/task/rds-ca-2019-2015-root.pem",
+            ssl_ca=rds_ca_cert,
             ssl_verify_cert=True,
         )
     except Exception as e:
@@ -179,16 +207,35 @@ def handler(event, context):
     validate_event(event)
     validate_envvars()
 
+    if "mysql_user_password_secret_name" in event.keys():
+        mysql_user_password_source = event["mysql_user_password_secret_name"]
+        mysql_user_password_source_type = "secretsmanager"
+    else:
+        mysql_user_password_source = event["mysql_user_password_parameter_name"]
+        mysql_user_password_source_type = "ssm"
+
+    if "RDS_MASTER_PASSWORD_SECRET_NAME" in os.environ:
+        mysql_master_password_source = os.environ["RDS_MASTER_PASSWORD_SECRET_NAME"]
+        mysql_master_password_source_type = "secretsmanager"
+    else:
+        mysql_master_password_source = os.environ["RDS_MASTER_PASSWORD_PARAMETER_NAME"]
+        mysql_master_password_source_type = "ssm"
+
     mysql_user_username = event["mysql_user_username"]
-    mysql_user_password_parameter_name = event["mysql_user_password_parameter_name"]
+    mysql_master_username = os.environ["RDS_MASTER_USERNAME"]
     database = os.environ["RDS_DATABASE_NAME"]
 
     logger.info(f"Updating {mysql_user_username}")
-
     pw = generate_password()
-    update_password_parameter(pw, mysql_user_password_parameter_name)
-
-    user_exists = check_user_exists(mysql_user_username)
+    update_password_source(
+        pw, mysql_user_password_source, mysql_user_password_source_type
+    )
+    user_exists = check_user_exists(
+        mysql_master_username,
+        mysql_user_username,
+        mysql_master_password_source,
+        mysql_master_password_source_type,
+    )
     if user_exists:
         logger.info(
             f"User {mysql_user_username} already exists in MySQL, will update password"
@@ -198,23 +245,32 @@ def handler(event, context):
     execute_statement(
         "CREATE USER IF NOT EXISTS '{}'@'%' IDENTIFIED BY '{}';".format(
             mysql_user_username, pw
-        )
+        ),
+        mysql_master_username,
+        mysql_master_password_source,
+        mysql_master_password_source_type,
     )
     execute_statement(
-        "SET PASSWORD FOR '{}'@'%' = PASSWORD('{}');".format(mysql_user_username, pw)
+        "SET PASSWORD FOR '{}'@'%' = PASSWORD('{}');".format(mysql_user_username, pw),
+        mysql_master_username,
+        mysql_master_password_source,
+        mysql_master_password_source_type,
     )
     execute_statement(
-        "GRANT ALL ON `{}`.* to '{}'@'%';".format(database, mysql_user_username)
+        "GRANT ALL ON `{}`.* to '{}'@'%';".format(database, mysql_user_username),
+        mysql_master_username,
+        mysql_master_password_source,
+        mysql_master_password_source_type,
     )
 
     test_result = test_connection(
-        mysql_user_username, mysql_user_password_parameter_name
+        mysql_user_username, mysql_user_password_source, mysql_user_password_source_type
     )
     if test_result:
         logger.info(
-            f"Password rotation complete: MySQL user {mysql_user_username} succesfully logged in using password from SSM parameter {mysql_user_password_parameter_name}"
+            f"Password rotation complete: MySQL user {mysql_user_username} succesfully logged in using password from source {mysql_user_password_source} in {mysql_user_password_source_type}"
         )
     else:
         raise ValueError(
-            f"Password rotation failed: MySQL user {mysql_user_username} failed to login using password from SSM parameter {mysql_user_password_parameter_name}"
+            f"Password rotation failed: MySQL user {mysql_user_username} failed to login using password from source {mysql_user_password_source} in {mysql_user_password_source_type}"
         )
